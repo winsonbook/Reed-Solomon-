6a7e956fe8a1b39963290c2cf4418e862b83aa83
Azam <38999092+hercules8088@users.noreply.github.com>
2019-08-23 02:46:08 +0530

    Update S_to_Sigma.v

--- S_to_Sigma.v
+++ S_to_Sigma.v
@@ -14,6 +14,10 @@ module S_to_Sigma(
     output reg [7:0] s1,
     output reg [7:0] s2,
     output reg [7:0] s3
+//    output [7:0] dlt,
+//    output [7:0] dlt1,
+//    output [7:0] dlt2,
+//    output [7:0] dlt3
     );
 
 reg [7:0] S1,S2,S3,S4,S5,S6;
@@ -57,58 +61,46 @@ always@(state,signal,OS_T0,OS_T1,OS_T2,OS_T3)
 				begin 
 					load = 1; 
 					if(signal) 
-						next_state = S_T3;
+						next_state = S_T0;
 				end
 			S_T0: 
 				begin  
 					T0 = 1;
-					if((OS_T0 == 2'b10)||(OS_T0 == 2'b01)) next_state = S_idle;
+					if(OS_T0 == 2'b01) next_state = S_T1;
+					if(OS_T0 == 2'b10) next_state = S_idle;
 				end	
 			S_T1:	   
 				begin 
 					T1 = 1;
-					if(OS_T1 == 2'b01) next_state = S_T0;
+					if(OS_T1 == 2'b01) next_state = S_T2;
 					if(OS_T1 == 2'b10) next_state = S_idle;
 				end
 			S_T2:	   
 				begin 
 					T2 = 1;
-					if(OS_T2 == 2'b01) next_state = S_T1;
+					if(OS_T2 == 2'b01) next_state = S_T3;
 					if(OS_T2 == 2'b10) next_state = S_idle;					
 				end	
 			S_T3:
 				begin
 					T3 = 1;
-					if(OS_T3 == 2'b01) next_state = S_T2;
-					if(OS_T3 == 2'b10) next_state = S_idle;
+					next_state = S_idle;
 				end
 			default: next_state = S_idle;
 		endcase
 	end
+wire [7:0] out1,out2,out12,out22,out13,out23;
+equation e1 (.S1(S1),.S2(S2),.S3(S3),.S4(S4),.out1(out1),.out2(out2));
+equation e2 (.S1(S3),.S2(S4),.S3(S5),.S4(S6),.out1(out12),.out2(out22));
+equation e3 (.S1(S2),.S2(S3),.S3(S4),.S4(S5),.out1(out13),.out2(out23));
 
-wire [7:0] delta,delta1,delta2,m1,m2,m3,m4,m5,m6,i_delta,out1,out2;
-multiply M1 (.X(m1),.A(S1),.B(S3));
-multiply M2 (.X(m2),.A(S2),.B(S2));
-assign delta = m1^m2;
-
-multiply M3 (.X(m3),.A(S1),.B(S4));
-multiply M4 (.X(m4),.A(S2),.B(S3));
-assign delta1 = m3^m4;
-
-multiply M5 (.X(m5),.A(S2),.B(S4));
-multiply M6 (.X(m6),.A(S3),.B(S3));
-assign delta2 = m5^m6;
-
-Inverse_8_bit I1 (.x(delta),.y(i_delta));
-multiply M7 (.X(out1),.A(i_delta),.B(delta1));
-multiply M8 (.X(out2),.A(i_delta),.B(delta2));
-
-
-wire [7:0] dlt,dlt1,dlt2,dlt3,out3,out4,out5,i_dlt;
+wire [7:0] dlt,dlt1,dlt2,dlt3;
+wire [7:0] out3,out4,out5,i_dlt;
 Determinant d1 (.d1(S1),.d2(S2),.d3(S3),.d4(S2),.d5(S3),.d6(S4),.d7(S3),.d8(S4),.d9(S5),.out(dlt));
-Determinant d2 (.d1(S1),.d2(S2),.d3(S4),.d4(S2),.d5(S3),.d6(S5),.d7(S3),.d8(S4),.d9(S6),.out(dlt1));
+Determinant d2 (.d1(S4),.d2(S2),.d3(S3),.d4(S5),.d5(S3),.d6(S4),.d7(S6),.d8(S4),.d9(S5),.out(dlt1));
 Determinant d3 (.d1(S1),.d2(S4),.d3(S3),.d4(S2),.d5(S5),.d6(S4),.d7(S3),.d8(S6),.d9(S5),.out(dlt2));
-Determinant d4 (.d1(S4),.d2(S2),.d3(S3),.d4(S5),.d5(S3),.d6(S4),.d7(S6),.d8(S4),.d9(S5),.out(dlt3));
+Determinant d4 (.d1(S1),.d2(S2),.d3(S4),.d4(S2),.d5(S3),.d6(S5),.d7(S3),.d8(S4),.d9(S6),.out(dlt3));
+
 Inverse_8_bit I2 (.x(dlt),.y(i_dlt));
 multiply M9 (.X(out3),.A(i_dlt),.B(dlt1));
 multiply M10 (.X(out4),.A(i_dlt),.B(dlt2));
@@ -135,36 +127,20 @@ always@(posedge clk)
 				OS_T2 = 2'b00;
 				OS_T3 = 2'b00;
 				ready = 0;
-			end
-			
-		else if(T3)
-			begin
-				if(dlt == 8'b00000000)
-					begin 
-						OS_T3 = 2'b01;
-						s1 = 8'bxxxxxxxx;
-						s2 = 8'bxxxxxxxx;
-						s3 = 8'bxxxxxxxx;
-					end
-				else
-					s1 = out3;
-					s2 = out4;
-					s3 = out5;
-					OS_T3 = 2'b10;
-			end		
+			end	
 		
-		else if(T2)
+		else if(T0)
 			begin
-				if(delta == 8'b00000000)
-					OS_T2 = 2'b01;
-				else
+				if((S1 == 8'b00000000)&(S2 == 8'b00000000)&(S3 == 8'b00000000)&(S4 == 8'b00000000)&(S5 == 8'b00000000)&(S6 == 8'b00000000))
 					begin
-						s1 = out1;
-						s2 = out2;
+						s1 = 8'b00000000;
+						s2 = 8'b00000000;
 						s3 = 8'b00000000;
 						ready = 1;
-						OS_T2 = 2'b10;
+						OS_T0 = 2'b10;
 					end
+				else
+					OS_T0 = 2'b01;
 			end
 		
 		else if(T1)
@@ -180,20 +156,63 @@ always@(posedge clk)
 				else
 					OS_T1 = 2'b01;
 			end
-			
-		else if(T0)
+		
+		else if(T2)
 			begin
-				if((S1 == 8'b00000000)&(S2 == 8'b00000000)&(S3 == 8'b00000000)&(S4 == 8'b00000000)&(S5 == 8'b00000000)&(S6 == 8'b00000000))
+				if((out1==out12)&&(out1==out13)&&(out2==out22)&&(out2==out23))
 					begin
-						s1 = 8'b00000000;
-						s2 = 8'b00000000;
+						s1 = out1;
+						s2 = out2;
 						s3 = 8'b00000000;
 						ready = 1;
-						OS_T0 = 2'b10;
+						OS_T2 = 2'b10;
 					end
 				else
-					OS_T0 = 2'b01;
+					OS_T2 = 2'b01;
 			end
+			
+	   else if(T3)
+			begin
+				if(dlt == 8'b00000000)
+					begin 
+						OS_T3 = 2'b01;
+						s1 = 8'bxxxxxxxx;
+						s2 = 8'bxxxxxxxx;
+						s3 = 8'bxxxxxxxx;
+					end
+				else
+					s1 = out3;
+					s2 = out4;
+					s3 = out5;
+					ready = 1;
+					OS_T3 = 2'b10;
+			end	
 	end
 	
+endmodule
+
+module equation(
+input [7:0] S1,
+input [7:0] S2,
+input [7:0] S3,
+input [7:0] S4,
+output [7:0] out1,
+output [7:0] out2
+);
+wire [7:0] delta,delta1,delta2,m1,m2,m3,m4,m5,m6,i_delta;
+multiply M1 (.X(m1),.A(S1),.B(S3));
+multiply M2 (.X(m2),.A(S2),.B(S2));
+assign delta = m1^m2;
+
+multiply M3 (.X(m3),.A(S1),.B(S4));
+multiply M4 (.X(m4),.A(S2),.B(S3));
+assign delta1 = m3^m4;
+
+multiply M5 (.X(m5),.A(S2),.B(S4));
+multiply M6 (.X(m6),.A(S3),.B(S3));
+assign delta2 = m5^m6;
+
+Inverse_8_bit I1 (.x(delta),.y(i_delta));
+multiply M7 (.X(out1),.A(i_delta),.B(delta1));
+multiply M8 (.X(out2),.A(i_delta),.B(delta2));
 endmodule
